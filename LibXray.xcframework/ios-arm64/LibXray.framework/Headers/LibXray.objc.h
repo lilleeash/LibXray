f598aed3f6a14b3a32fab42715ff279ae077678a
Darya Zhitova <lilleeash@icloud.com>
2024-05-28 11:40:07 +0300

    init commit

---
+++ LibXray.xcframework/ios-arm64/LibXray.framework/Headers/LibXray.objc.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f29c712fae73d74496c41115f24c60dfca12101bbf7de3c28cb743926eacf521
+size 3970
---
+++ LibXray.xcframework/macos-arm64_x86_64/LibXray.framework/Versions/A/Headers/LibXray.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:84f45602e4ee0b2c0cf3623a1acb73d23b2aac636e3a663649bb753a75066ef0
+size 272
